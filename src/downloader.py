c074f2e179c702ee1e7240e9252ce6f04817ffa2
Kosio <kosiokarchev@gmail.com>
2017-10-12 19:36:30 +0100

    initial commit

---
+++ src/downloader.py
@@ -0,0 +1,26 @@
+import subprocess
+
+
+output_path = 'test.csv'
+
+username = 'wuijts'
+password = 'G984587L'
+url = 'http://gavo.mpa-garching.mpg.de/MyMillennium/'
+
+limit = 1000000
+columns = ['galaxyId', 'type',
+           'phKey', 'x', 'y', 'z', 'velX', 'velY', 'velZ',
+           'redshift', 'lookBackTime',
+           'rvir', 'bulgeSize', 'stellarDiskRadius',
+           'mvir', 'coldGas', 'stellarMass', 'bulgeMass', 'diskMass', 'hotGas', 'ejectedMass', 'blackHoleMass', 'icmStellarMass',
+           'sfr', 'sfrBulge',
+           'centralMVir', 'centralRvir', 'distanceToCentralGalX', 'distanceToCentralGalY', 'distanceToCentralGalZ',
+           'treeId', 'descendantId', 'mainLeafId', 'treeRootId', 'firstProgenitorId', 'nextProgenitorId', 'lastProgenitorId', 'haloId', 'subHaloId', 'fofCentralId', 'fofSubhaloId']
+table = 'Henriques2015a..MRscPlanck1'
+sql = 'SELECT TOP {limit} {cols} FROM {table}'
+
+SQL = sql.format(limit=limit, cols=','.join(columns), table=table)
+full_url = url + '?action=doQuery&SQL=' + SQL
+
+wget = ('wget', '--http-user='+username, '--http-passwd='+password,'-O', output_path, full_url)
+subprocess.run(wget, check=True)
