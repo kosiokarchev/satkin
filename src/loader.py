4cced1854572b3fa2edfa179f4f7398cadff463b
Kosio <kosiokarchev@gmail.com>
2017-10-18 23:41:17 +0100

    use external sql files

--- src/loader.py
+++ src/loader.py
@@ -1,25 +1,23 @@
-import sqlite3
-import numpy as np
+from __future__ import division, print_function
 
-FNAMES = ['data/s34.csv', 'data/s38.csv', 'data/s45.csv']
-# FNAMES = ['/users/kosio/s45s.csv']
+import sqlite3
+from astropy.table import Table
 
 db = sqlite3.connect('data/data.db')
+db.execute('PRAGMA busy_timeout = 10000')
 
 with db:
-    db.execute('''CREATE TABLE IF NOT EXISTS cube (
-    galaxyId INT PRIMARY KEY,
-    haloId INT, subHaloId INT, fofCentralId INT, fofSubhaloId INT,
-    centralMVir REAL, centralRvir REAL, distanceToCentralGalX REAL, distanceToCentralGalY REAL, distanceToCentralGalZ REAL,
-    type INT,
-    x REAL, y REAL, z REAL, velX REAL, velY REAL, velZ REAL, stellarSpinX REAL, stellarSpinY REAL, stellarSpinZ REAL,
-    mvir REAL, rvir REAL, stellarMass REAL, sfr REAL
-    ) WITHOUT ROWID''')
+    with open('src/sql/cube_create.sql') as f:
+        query = f.read()
+    db.execute(query)
 
-for fname in FNAMES:
-    print('Reading file '+fname)
-    rows = np.loadtxt(fname, skiprows=1, delimiter=',')
-    print('Found {} rows.'.format(len(rows)))
-    with db:
-        db.executemany('INSERT INTO cube VALUES ('+(','.join(['?']*24))+')', rows)
-    print('File added to database.')
+FNAME = 'data/cube.fits'
+
+print('Reading file '+FNAME)
+t = Table.read(FNAME)
+print('Found {} rows.'.format(len(t)))
+
+query = 'INSERT INTO cube ({}) VALUES ({})'.format(t.columns, ','.join(['?']*24))
+with db:
+    db.executemany(query, t)
+print('File added to database.')
