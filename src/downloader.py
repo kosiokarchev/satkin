e308c04bc91c5820e5006ff013101ba0612b8f58
Kosio <kosiokarchev@gmail.com>
2017-10-25 19:56:21 +0100

    streaming download in chunks

--- src/downloader.py
+++ src/downloader.py
@@ -1,21 +1,117 @@
 #!/usr/bin/env python3
-import subprocess
+import os
+from time import time
+import requests
 
 
-def download(sql, fname):
-    with open('credentials.txt', encoding='ascii') as f:
-        usr, pwd = f.read().replace('\n', '').split(':')
-    url = 'http://gavo.mpa-garching.mpg.de/MyMillennium/?action=doQuery&SQL='+sql
+class Query:
+    def __init__(self, cols, table, where=None, order=None):
+        self.cols = list(cols)
+        self.table = table
+        self.where = where if where is not None else ''
+        self.order = list(order) if order is not None else []
 
-    wget = ('wget', '--http-user=' + usr, '--http-passwd=' + pwd, '-O', fname, url)
-    subprocess.run(wget, check=True)
+    def __call__(self):
+        return 'SELECT {cols}\nFROM {table}{where}{order}'.format(
+            cols = ','.join(self.cols),
+            table = self.table,
+            where = '\nWHERE '+self.where if self.where else '',
+            order = '\nORDER BY ' + ','.join(self.order) if self.order else ''
+        )
 
 
-def gen_select(cols, table, where='', top=0):
-    return 'SELECT {top} {cols} FROM {table} {where}'.format(
-        top=('TOP '+str(top)) if top else '',
-        cols=', '.join(cols), table=table,
-        where=('WHERE '+where) if where else '')
+class Downloader:
+    def __init__(self, query, out, index='galaxyId'):
+        self.query = query
+        self.out = os.path.abspath(out)
+        self.index = index
+
+    @staticmethod
+    def load_pass():
+        pf = os.path.join(os.path.dirname(__file__), 'credentials.txt')
+        with open(pf, encoding='ascii') as f:
+            usr, pwd = f.read().replace('\n', '').split(':')
+        return usr, pwd
+
+    def go(self):
+        baseUrl = 'http://gavo.mpa-garching.mpg.de/MyMillennium/'
+
+        if self.index not in self.query.cols:
+            self.query.cols.append(self.index)
+        if not (self.query.order and self.query.order[0] == self.index):
+            self.query.order = [self.index+' OPTION (FAST 100000)'] + self.query.order
+
+        indexindex = self.query.cols.index(self.index)
+        maxindex = 0
+        basewhere = self.query.where
+
+        with requests.Session() as sess:
+            sess.auth = Downloader.load_pass()
+            sess.stream = True
+
+            print('='*80)
+            print('='*80)
+            print(self)
+            print('='*80)
+            print('='*80)
+
+            while True:
+                self.query.where = '{}>{} AND ({})'.format(self.index, maxindex, basewhere)
+
+                q = self.query()
+
+                start_time = time()
+
+                r = sess.get(baseUrl, params={'action': 'doQuery', 'SQL': q})
+                r.raise_for_status()
+
+                lines = r.iter_lines(decode_unicode=True)
+
+                line = next(lines)
+                if not line == '#OK':
+                    raise RuntimeError('Query not OK')
+
+                nlines = 1
+                nchars = len(line)
+                with open(self.out, 'a') as f:
+                    for line in lines:
+                        if line[0] == '#':
+                            pass
+                            # print('Ignoring as comment:', line)
+                        else:
+                            f.write(line + '\n')
+                            maxindex = line.split(',')[indexindex]
+
+                        nlines += 1
+                        nchars += len(line)
+                        dt = time() - start_time
+
+
+                        if nlines % 10000 == 0:
+                            msg = '({:,.0f} lines)'.format(nlines)
+                            msg += ' [{:.0f}s]'.format(dt)
+
+                            if nchars < 1000000:
+                                msg += ' {:.0f} kc'.format(nchars / 1000)
+                            else:
+                                msg += ' {:.0f} Mc'.format(nchars / 1000000)
+
+                            print(msg+'\u001b[0K\r', end='', flush=True)
+
+                print()
+                print('Last line is', line)
+                print('maxindex is', maxindex)
+                print('=' * 80)
+
+                if line == '#OK':
+                    break
+
+        print('DOWNLOAD COMPLETE!!')
+        print('Data saved in', self.out)
+
+
+    def __repr__(self):
+        return self.query() + '\n--> ' + self.out
 
 
 TABLES = {'H15-cube': 'Henriques2015a..MRscPlanck1',
@@ -38,6 +134,27 @@ COLUMNS = {
     ]
 }
 
-
 if __name__ == '__main__':
-    download(gen_select(COLUMNS['H15-cube'], TABLES['H15-cube'], top=10000), 'test.csv')
+    cols_central = [
+        'galaxyId',
+        'x', 'y', 'z', 'velX', 'velY', 'velZ', 'stellarSpinX', 'stellarSpinY',
+        'stellarSpinZ',
+        'mvir', 'rvir', 'stellarMass', 'sfr',
+    ]
+    cols_satellites = [
+        'galaxyId', 'fofCentralId',
+        'velX', 'velY', 'velZ',
+        'distanceToCentralGalX', 'distanceToCentralGalY', 'distanceToCentralGalZ'
+    ]
+
+    snapnums = (38, 34, 45)
+
+    for snapnum in snapnums:
+        for typewhere, cols in zip(('=0', '>0'), (cols_central, cols_satellites)):
+            where = 'snapnum={} AND type{}'.format(snapnum, typewhere)
+            q = Query(cols, TABLES['H15-cube'], where)
+
+            fname = 'data/cube{}{}.csv'.format(snapnum, typewhere)
+
+            d = Downloader(q, fname)
+            d.go()
