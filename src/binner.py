0070964d7ed349b6e2dee00b350ce410612ae96e
Kosio <kosiokarchev@gmail.com>
2017-11-09 23:53:41 +0000

    first step in generalising

---
+++ src/binner.py
@@ -0,0 +1,160 @@
+from lib import *
+import pickle
+
+
+def binX(t, x, y, nbins=20, bins=None, funcsx=None, funcsy=None):
+    if funcsx is None:
+        funcsx = {'mean': np.mean, 'std': np.std}
+    elif callable(funcsx):
+        funcsx = {'stat': funcsx}
+
+    if funcsy is None:
+        funcsy = {'mean': np.mean, 'std': np.std}
+    elif funcsy is True:
+        funcsy = funcsx
+    elif callable(funcsy):
+        funcsy = {'stat': funcsy}
+
+
+    if bins is not None:
+        width = bins[1] - bins[0]
+        left = bins[0]
+        nbins = len(bins) - 1
+    else:
+        left = np.min(t[x])
+        width = (np.max(t[x]) - left) / nbins
+
+    t['bin'] = np.floor_divide(t[x] - left, width).astype(int)
+    g = t.group_by('bin')
+
+    statsx = []
+    statsy = []
+    for i in range(len(g.groups.keys)):
+        if 0 <= g.groups.keys[i]['bin'] < nbins:
+            statsx.append([f(g[x].groups[i]) for f in funcsx.values()])
+            statsy.append([f(g[y].groups[i]) for f in funcsy.values()])
+
+    return ({name: stat for name, stat in zip(funcsx.keys(), np.array(statsx).transpose())},
+            {name: stat for name, stat in zip(funcsy.keys(), np.array(statsy).transpose())})
+
+def bin2D(t, x, y, nbinsx=20, nbinsy=20, binsx=None, binsy=None, funcsx=None, funcsy=None):
+    print('Binning in', x)
+    binx = binX(t, x, y, nbinsx, binsx, funcsx, funcsy)
+
+    print('Binning in', y)
+    biny = binX(t, y, x, nbinsy, binsy, funcsx, funcsy)
+    biny = biny[1], biny[0]
+
+    return binx, biny
+
+
+class bin2d:
+    def __init__(self, t=None, x=None, y=None, binx=None, biny=None):
+        self.binx = binx
+        self.biny = biny
+
+        self.t = t
+        self.x = x
+        self.y = y
+
+    def load(self, fname):
+        with open(fname, 'rb') as f:
+            binx, biny = pickle.load(f, encoding='bytes')
+
+        for b in binx, biny:
+            for c in b:
+                for key in tuple(c.keys()):
+                    if isinstance(key, bytes):
+                        c[key.decode()] = c[key]
+                        del c[key]
+
+        self.binx, self.biny = binx, biny
+    def save(self, fname):
+        with open(fname, 'wb') as f:
+            pickle.dump((self.binx, self.biny), f)
+
+    def compute(self, binsx, binsy):
+        self.binx, self.biny = bin2D(
+            self.t, self.x, self.y, binsx=binsx, binsy=binsy,
+            funcsx={'mean': np.mean, 'std': np.std, 'N': len},
+            funcsy={'mean': np.mean, 'std': np.std, 'median': np.median,
+                    'p2': percentile_wrapper(2),
+                    'p16': percentile_wrapper(16),
+                    'p84': percentile_wrapper(84),
+                    'p98': percentile_wrapper(98)})
+
+    def plot_data(self, sparse=1, label='_nolegend_'):
+        X, Y = self.t[self.x], self.t[self.y]
+        if sparse > 1:
+            r = (np.random.random(int(len(self.t) / sparse)) * len(self.t)).astype(int)
+            print(len(r))
+            X, Y = X[r], Y[r]
+        plt.plot(X, Y, 'r,', zorder=-1, label=label)
+    def plot_bins(self,
+                  label1='x binning', label2='y binning',
+                  clr1='blue', clr2='yellow',
+                  narrow=True, wide=False, median=False, ebars=False, mean=True):
+        def getkw(o, i=None):
+            return {} if o is True else getkw(o[i]) if isinstance(o, (list, tuple)) else o
+
+
+        defaults = [
+            [ # wide
+                dict(color=clr1, alpha=0.25, label='_nolegend_'),
+                dict(color=clr2, alpha=0.25, label='_nolegend_')
+            ],
+            [ # narrow
+                dict(color=clr1, alpha=0.5, label=label1),
+                dict(color=clr2, alpha=0.5, label=label2),
+            ],
+            [ # median
+                dict(marker='o', linestyle='None', color=clr1, markeredgecolor='red'),
+                dict(marker='o', linestyle='None', color=clr2, markeredgecolor='red')
+            ],
+            [dict(color='black', linestyle='None')]*2, # std
+            [dict(color='black')]*2  # mean
+        ]
+
+        ds = (wide, narrow, median, ebars, mean)
+        def ev(i, j, func, *args):
+            ukw = getkw(ds[j], i)
+            if ukw is not False:
+                kw = defaults[j][i]
+                kw.update(ukw)
+                func(*args, **kw)
+
+        for i, (x, y), (xm, ym), fill in zip(
+                (0, 1),
+                (self.binx, self.biny),
+                (('mean', 'median'), ('median', 'mean')),
+                (plt.fill_between, plt.fill_betweenx)):
+            fillx = x if i==0 else y
+            filly = y if i==0 else x
+
+            ev(i, 0, fill, fillx['mean'], filly['p2'], filly['p98'])
+            ev(i, 1, fill, fillx['mean'], filly['p16'], filly['p84'])
+            ev(i, 2, plt.plot, x[xm], y[ym])
+            ev(i, 3, plt.errorbar, x['mean'], y['mean'], y['std'], x['std'])
+            ev(i, 4, plt.plot, x['mean'], y['mean'])
+    def plot_amatch(self, label='abundance matching'):
+        X, Y = self.t[self.x].copy(), self.t[self.y].copy()
+        X.sort(), Y.sort()
+        plt.plot(X, Y, 'r-', label=label)
+    def plot(self, label1, label2, sparse=1, bins=True, data=False, amatch=False):
+        if data:
+            self.plot_data(sparse)
+        if bins:
+            self.plot_bins(label1, label2)
+        if amatch:
+            self.plot_amatch()
+
+
+def build3Dbins(sn):
+    fname = FILES['masses'](sn)
+    print('Loading', fname)
+    t = Table.read(fname)
+    t = deal(t)
+
+    b = bin2d(t, 'mv', 'ms')
+    b.compute(PLOTDATA['bins']['mvir'], PLOTDATA['bins']['stellarMass'])
+    b.save(FILES['3dbins'](sn))
---
+++ src/proclib.py
@@ -0,0 +1,191 @@
+from lib import *
+
+
+def combine(sn):
+    print('Loading tables ' + str(sn) + ' ...')
+    centrals = Table.read('data/cube' + str(sn) + '=0.fits', format ='fits')
+    sats = Table.read('data/cube' + str(sn) + '>0.fits', format ='fits')
+
+    print('Joining ' + str(sn) + ' ...')
+    centrals.rename_column('galaxyId', 'fofCentralId')
+    sats = join(sats, centrals, 'fofCentralId', table_names=['s', 'c']) #column names like name_s/name_c
+
+    print('Calculating velocities and distances ' + str(sn) + ' ...')
+    sats['vpecx'] = sats['velX_s'] - sats['velX_c']
+    sats['vpecy'] = sats['velY_s'] - sats['velY_c']
+    sats['vpecz'] = sats['velZ_s'] - sats['velZ_c']
+    sats['vpec'] = np.sqrt(sats['vpecx']**2+sats['vpecy']**2+sats['vpecz']**2) #magn of peculiar velocity
+
+    sats = sats['fofCentralId', 'galaxyId', 'mvir', 'stellarMass', 'vpecx', 'vpecy', 'vpecz', 'vpec', 'distanceToCentralGalX', 'distanceToCentralGalY', 'distanceToCentralGalZ']
+    sats.rename_column('distanceToCentralGalX', 'rx')
+    sats.rename_column('distanceToCentralGalY', 'ry')
+    sats.rename_column('distanceToCentralGalZ', 'rz')
+    sats['r'] = np.sqrt(sats['rx']**2+sats['ry']**2+sats['rz']**2)
+
+    print('Writing tables ' + str(sn) + ' ...')
+    sats.write('data/sats' + str(sn) + '.fits', format ='fits', overwrite=True)
+
+
+def get_nums(sn):
+    print(sn)
+    print('Loading...')
+    s = Table.read('data/cube' + str(sn) + '>0.fits')
+
+    print('Grouping...')
+    g = s.group_by('fofCentralId').groups
+    s = Table(names=('fofCentralId', 'num_sat'),
+              data=(
+              g.keys['fofCentralId'].data, g.indices[1:] - g.indices[:-1]))
+
+    print('Loading...')
+    c = Table.read('data/cube' + str(sn) + '=0.fits')
+    c.rename_column('galaxyId', 'fofCentralId')
+
+    print('Joining...')
+    s = join(s, c, 'fofCentralId')['fofCentralId', 'stellarMass', 'mvir', 'num_sat']
+
+    print('Writing...')
+    s.write('data/nums' + str(sn) + '.fits', overwrite = True)
+
+
+def joinsats(sn, clean=False, outname='data/satsnum{}.fits', **kwargs):
+    print(sn)
+    print('Loading sats')
+    sats = Table.read('data/sats{}.fits'.format(sn))
+    print('Loading nums')
+    nums = Table.read('data/nums{}.fits'.format(sn))['fofCentralId', 'num_sat']
+
+    if clean:
+        nums = nums[np.where(nums['num_sat'] > 2)]
+
+    print('Joining...')
+    sats = join(nums, sats, 'fofCentralId')
+
+    print('Writing...')
+    sats.write(outname.format(sn), **kwargs)
+
+
+class Procedure:
+    def __init__(self, sn, observe):
+        self.sn = sn
+        self.observe = observe
+        self.stdev = None
+
+    def restore(self):
+        raise NotImplementedError
+    def load(self):
+        raise NotImplementedError
+    def plot(self, *args, **kwargs):
+        raise NotImplementedError
+    def predict(self):
+        raise NotImplementedError
+    def go(self):
+        raise NotImplementedError
+
+
+
+class SWProcedure(Procedure):
+    def __init__(self, sn, observe):
+        Procedure.__init__(self, sn, observe)
+        self.sats = None
+
+    def restore(self, mname='stellarMass'):
+        self.stdev = Table.read(FILES['sigmas'](sn, mname, self.observe))
+
+    def load(self):
+        print('Loading table...')
+        t = Table.read(FILES['sats'](self.sn))
+        t = t[np.where(t['stellarMass'] > 1e6)]
+        t['mv'] = 10 + np.log10(t['mvir'])
+        t['ms'] = 10 + np.log10(t['stellarMass'])
+        self.sats = t
+
+    def bin(self, mname, left, width):
+        self.sats['bin'] = np.floor_divide(self.sats[mname] - left, width)
+        binned = self.sats.group_by('bin')
+
+        res = binned['bin', 'vpec', 'vpecx', 'vpecy', 'vpecz'].groups.aggregate(np.std)
+
+        res.rename_column('vpec', 'sigma')
+        res.rename_column('vpecx', 'sigmax')
+        res.rename_column('vpecy', 'sigmay')
+        res.rename_column('vpecz', 'sigmaz')
+
+        res['mean'] = binned['vpec'].groups.aggregate(np.mean)
+        res['median'] = binned['vpec'].groups.aggregate(np.median)
+        res[mname] = left + (res['bin'] + 0.5) * width
+        res['N'] = binned['fofCentralId'].groups.aggregate(lambda x: len(np.unique(x)))
+
+        self.stdev = res
+
+    def plot(self, mname, xlim, xlabel='$\log_{10}(M / M_\odot)$', sparse=333):
+        print('Plotting...')
+        fig, ax = plt.subplots(2, 1, sharex='col', squeeze=True,
+                               gridspec_kw={'height_ratios': [3, 1],
+                                            'hspace': 0})
+
+        plt.sca(ax[0])
+        r = np.random.randint(int(len(sats) / sparse))
+        plt.plot(self.sats[mname][r], self.sats['vpec'][r], ',', label='_nolegend_')
+
+        X = self.stdev[mname]
+        Y = self.stdev
+        plt.plot(X, Y['sigma'], '-r', linewidth=3, label='$\sigma_{3D}$')
+        plt.plot(X, Y['sigmax'], '-y', linewidth=2, label='$\sigma_{los}$')
+        plt.plot(X, Y['sigmay'], '-y', linewidth=2, label='_nolegend_')
+        plt.plot(X, Y['sigmaz'], '-y', linewidth=2, label='_nolegend_')
+        plt.plot(X, Y['mean'], '-g', linewidth=3, label='$\langle |v| \\rangle$')
+        plt.semilogy()
+
+        plt.legend(loc='lower right')
+
+        plt.ylabel('Speed, km/s')
+
+
+        print('Plotting...')
+        plt.sca(ax[1])
+        plt.plot(X, Y / X)
+        plt.plot(xlim, [np.sqrt(3)]*2, ':')
+        plt.plot(xlim, [np.sqrt(8 / np.pi)]*2, ':')
+
+        plt.xlabel(xlabel)
+        plt.ylabel('$v_{3D}$ / $\sigma_{los}$')
+
+        plt.ylim(1, 2)
+        plt.xlim(xlim)
+
+    def go(self, mnames=('mvir', 'stellarMass'), write=True):
+        self.load()
+
+        if self.observe:
+            self.sats = Observer.get().observe(self.sats)
+
+        for mname in mnames:
+            xlim = PLOTDATA['bins'][mname][0], PLOTDATA['bins'][mname][-1]
+            self.bin(mname, xlim[0], BINWIDTH)
+
+            if write:
+                self.stdev.write(FILES['sigmas'](self.sn, mname, self.observe), overwrite=True)
+
+            self.plot(mname, xlim)
+            fname = FILES['plotsigmas'](sn)
+            plt.savefig(fname)
+            print('Figure saved:', fname)
+            plt.close()
+
+    def predict(self):
+        params = Regressor.get()[self.sn]
+        sn, amp, err_amp, exp, err_exp = params
+
+        data = Table.read(FILES['sigmas'](self.sn, 'stellarMass', self.observe))
+
+        data['mvir'] = np.power((data['sigmax'] / amp), 1 / exp)
+
+        data['relerr_mvir'] = np.sqrt(
+            ((1 / exp) * (err_amp / amp)) ** 2 + (
+            np.log(data['mvir']) * (err_exp / exp)) ** 2
+        )
+
+        return Data(mvir=data['mvir'], mstar=data['stellarMass'],
+                    relerr_mvir=data['relerr_mvir'],
+                    N=data['N'])
