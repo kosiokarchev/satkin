269d711c4c845fa3d488e14cd7295257b4e64df3
Kosio <kosiokarchev@gmail.com>
2017-11-22 00:16:23 +0000

    removed sql from git

--- src/sql/cube_create.sql
+++ src/sql/cube_create.sql
@@ -2,11 +2,13 @@ CREATE TABLE IF NOT EXISTS
 cube (
     galaxyId INT PRIMARY KEY,
     haloId INT, subHaloId INT, fofCentralId INT, fofSubhaloId INT,
-    centralMVir REAL, centralRvir REAL,
-    distanceToCentralGalX REAL, distanceToCentralGalY REAL, distanceToCentralGalZ REAL,
-    type INT,
+	centralMVir REAL, centralRvir REAL,
+	distanceToCentralGalX REAL, distanceToCentralGalY REAL, distanceToCentralGalZ REAL,
+	type INT,
     x REAL, y REAL, z REAL,
     velX REAL, velY REAL, velZ REAL,
     stellarSpinX REAL, stellarSpinY REAL, stellarSpinZ REAL,
-    mvir REAL, rvir REAL, stellarMass REAL, sfr REAL)
+    mvir REAL, rvir REAL, stellarMass REAL, sfr REAL,
+    redshift REAL
+)
 WITHOUT ROWID
