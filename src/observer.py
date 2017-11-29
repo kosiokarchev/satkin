9adb49175abb3fe30adee2c74ad528ccdb4fd264
Kosio <kosiokarchev@gmail.com>
2017-11-29 23:55:15 +0000

    inami observer with fitted params

--- src/observer.py
+++ src/observer.py
@@ -3,9 +3,19 @@ from scipy.special import erf
 import numpy.random as random
 
 
-def p(x):
-    y = 0.5*(1+erf(-(x-26)/2))
-    return y
+def prob_erf(x, m, s, A, H):
+    return H * 0.5*(1+erf((x-m) / s)) + A
+prob_erf_params = (25.718585789217027,
+                   -1.7715191963066255,
+                   0.044643065344693235,
+                   0.9564014441048382)
+def prob_fd(x, m, s, A, H):
+    return H / (1+np.exp((x-m) / s)) + A
+prob_fd_params = (25.708477496243546,
+                  0.7560238533784606,
+                  0.04035120415731956,
+                  0.9670816213373163)
+prob_func = lambda x: prob_erf(x, *prob_erf_params)
 
 class Observer:
     _observer = None
@@ -21,8 +31,8 @@ class Observer:
         self.mags = load_table('data/cones/cone.fits')
 
         rand = random.random(len(self.mags))
-        func = p(self.mags['ACS775'])
-        self.sample = self.mags[np.where(func > rand)]
+        prob = prob_func(self.mags['ACS775'])
+        self.sample = self.mags[np.where(prob > rand)]
 
         print('Observer prepared.')
 
