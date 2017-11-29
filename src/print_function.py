9a3e8edb51ae63f44d17db4c33eb25f934df02d5
Kosio <kosiokarchev@gmail.com>
2017-11-29 00:14:09 +0000

    just flush

--- src/print_function.py
+++ src/print_function.py
@@ -3,5 +3,4 @@ import sys
 
 def print(*args, **kwargs):
     sys.stdout.write(' '.join([str(a) for a in args])+(kwargs['end'] if 'end' in kwargs else ''))
-    if 'flush' in kwargs:
-        sys.stdout.flush()
+    sys.stdout.flush()
